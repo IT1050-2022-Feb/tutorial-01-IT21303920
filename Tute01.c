a8fca7b8a97bdd31c41308991dd54576193ee3b5
Mushtaq <99312305+IT21303920@users.noreply.github.com>
2022-02-09 07:23:57 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,23 @@
 #include <stdio.h>
 
 int main() {
+
+  //variable declaration
+  int a, b ;
+  float total , avg ;
   
+  //input
+  printf("Enter Marks\n");
+  scanf("%d" , &a);
+  scanf("%d" , &b);
+
+  //Calculations
+  total = a + b ;
+  avg = total / 2 ;
+  
+  //output 
+  printf("Average is : %.2f" ,avg) ;
+
   return 0;
 }
 
